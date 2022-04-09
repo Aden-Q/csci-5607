be9bd776d1126a415fbc88488374c04bbb045d80
aden-q <627374286@qq.com>
2022-04-08 21:23:46 -0500

    Update

--- hw2a/bin/CMakeFiles/3.23.0/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/qianzecheng/Code/5607/hw2a/bin/CMakeFiles/3.23.0/CompilerIdCXX/CMakeCXXCompilerId.cpp
--- hw2a/src/fshader2a.glsl
+++
@@ -1,9 +0,0 @@
-// fragment shader template
-
-#version 150
-in vec4 vcolor;
-out vec4 color;
-void main() 
-{
-	color = vcolor;  // set output color to interpolated color from vshader
-}
